bc1017e08034f672a25a4a9b8eb5112d03d37cbc
119551279 <65332382+119551279@users.noreply.github.com>
2026-03-26 15:39:03 +0800

    Create embedding_service.py backend service

---
+++ Python/embedding_service.py
@@ -0,0 +1,11 @@
+def embed(text):
+    """
+    Embedding function to convert text into embedding vector.
+    """
+    # Implementation here
+    pass
+
+
+if __name__ == '__main__':
+    sample_text = "This is a sample text"
+    print(embed(sample_text))
